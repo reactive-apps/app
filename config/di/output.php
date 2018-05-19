2934a16789011c3a95d999047672660efc5273fd
Cees-Jan Kiewiet <ceesjank@gmail.com>
2018-05-19 23:40:41 +0200

    Remove all STD* streams from the event loop on shutdown

--- config/di/output.php
+++ config/di/output.php
@@ -2,12 +2,20 @@
 
 use Clue\React\Stdio\Stdio;
 use React\EventLoop\LoopInterface;
+use ReactiveApps\Rx\Shutdown;
 use Symfony\Component\Console\Output\OutputInterface;
 use WyriHaximus\React\Symfony\Console\StdioOutput;
 
 return (function () {
     return [
-        OutputInterface::class => function (LoopInterface $loop) {
+        OutputInterface::class => function (LoopInterface $loop, Shutdown $shutdown) {
+            // Remove STD* streams from loop on shutdown
+            $shutdown->subscribe(null, null, function () use ($loop) {
+                $loop->removeReadStream(STDIN);
+                $loop->removeWriteStream(STDOUT);
+                $loop->removeWriteStream(STDERR);
+            });
+
             return new StdioOutput(new Stdio($loop), StdioOutput::VERBOSITY_NORMAL, true);
         },
     ];
