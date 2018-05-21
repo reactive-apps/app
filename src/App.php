f97f7c4665b9e6e26aea4d601ef956db863077bc
Cees-Jan Kiewiet <ceesjank@gmail.com>
2018-05-21 22:53:43 +0200

    Added logging to the app

--- src/App.php
+++ src/App.php
@@ -2,11 +2,14 @@
 
 namespace ReactiveApps;
 
+use Psr\Log\LoggerInterface;
 use React\EventLoop\LoopInterface;
 use ReactiveApps\Rx\Shutdown;
 use Silly\Application;
 use Symfony\Component\Console\Input\ArgvInput;
 use Symfony\Component\Console\Output\OutputInterface;
+use WyriHaximus\PSR3\CallableThrowableLogger\CallableThrowableLogger;
+use WyriHaximus\PSR3\ContextLogger\ContextLogger;
 
 final class App
 {
@@ -36,6 +39,11 @@ final class App
     private $output;
 
     /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /**
      * @var bool
      */
     private $booted = false;
@@ -45,37 +53,55 @@ final class App
      * @param Shutdown $shutdown
      * @param Application $application
      * @param OutputInterface $output
+     * @param LoggerInterface $logger
      */
-    public function __construct(LoopInterface $loop, Shutdown $shutdown, Application $application, OutputInterface $output)
+    public function __construct(LoopInterface $loop, Shutdown $shutdown, Application $application, OutputInterface $output, LoggerInterface $logger)
     {
         $this->loop = $loop;
         $this->shutdown = $shutdown;
         $this->application = $application;
         $this->output = $output;
+        $this->logger = new ContextLogger($logger, [], 'app');
+
+        $this->shutdown->subscribe(null, null, function () {
+            $this->logger->notice('Shutdown issued');
+        });
     }
 
     public function boot(array $argv)
     {
+        $this->logger->emergency('Booting');
         if ($this->booted === true) {
+            $this->logger->emergency('Can\'t be booted twice');
             return;
         }
         $this->booted = true;
+        $this->logger->emergency('Booting');
 
         $this->setUpSignals();
 
-        $this->loop->futureTick(function () use ($argv) {
+        $exitCode = null;
+        $this->loop->futureTick(function () use ($argv, &$exitCode) {
+            $this->logger->debug('Running');
             try {
-                $this->application->run(new ArgvInput($argv), $this->output);
+                $exitCode = $this->application->run(new ArgvInput($argv), $this->output);
             } catch (\Throwable $et) {
                 echo (string)$et;
+                CallableThrowableLogger::create($this->logger)($et);
             }
         });
 
+        $this->logger->debug('Starting loop');
         $this->loop->run();
+        $this->logger->debug('Execution completed with exit code' . $exitCode);
+
+        return $exitCode;
     }
 
     private function setUpSignals()
     {
+        $this->logger->debug('Setting up signals');
+
         $handler = function () {
             $this->shutdown->onCompleted();
         };
