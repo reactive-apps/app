dcb83dbee50c3118a3001a8bc6c515497bf1df2d
Cees-Jan Kiewiet <ceesjank@gmail.com>
2018-05-22 22:03:14 +0200

    Removed streams from the loop 0.3 after issuing shutdown

--- config/di/output.php
+++ config/di/output.php
@@ -11,9 +11,11 @@ return (function () {
         OutputInterface::class => function (LoopInterface $loop, Shutdown $shutdown) {
             // Remove STD* streams from loop on shutdown
             $shutdown->subscribe(null, null, function () use ($loop) {
-                $loop->removeReadStream(STDIN);
-                $loop->removeWriteStream(STDOUT);
-                $loop->removeWriteStream(STDERR);
+                $loop->addTimer(0.3, function () use ($loop) {
+                    $loop->removeReadStream(STDIN);
+                    $loop->removeWriteStream(STDOUT);
+                    $loop->removeWriteStream(STDERR);
+                });
             });
 
             return new StdioOutput(new Stdio($loop), StdioOutput::VERBOSITY_NORMAL, true);
