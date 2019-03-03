439b262bae1d1f6db0b476a903dabf7ff1c58ef1
Cees-Jan Kiewiet <ceesjank@gmail.com>
2019-03-03 19:42:55 +0100

    Made configuration location compatible with glob patterns

--- src/ConfigurationLocator.php
+++ src/ConfigurationLocator.php
@@ -8,8 +8,24 @@ final class ConfigurationLocator
 {
     public static function locate(): iterable
     {
-        foreach (get_in_packages_composer_path('extra.reactive-apps.config') as $path) {
-            yield from require $path;
+        yield from self::requires(get_in_packages_composer_path('extra.reactive-apps.config'));
+    }
+
+    private static function requires(iterable $files): iterable
+    {
+        foreach ($files as $file) {
+            yield from self::require($file);
         }
+    }
+
+    private static function require(string $file): iterable
+    {
+        if (\strpos($file, '*') !== false) {
+            yield from self::requires(\glob($file));
+
+            return;
+        }
+
+        yield from require $file;
     }
 }
