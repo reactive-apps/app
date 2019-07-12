284198e79a0ce65f024f1d31f0c5be94acf16abd
Cees-Jan Kiewiet <ceesjank@gmail.com>
2019-07-12 23:05:18 +0200

    Log errors when running commands to the logger

--- src/CommandCreator.php
+++ src/CommandCreator.php
@@ -3,55 +3,65 @@
 namespace ReactiveApps;
 
 use Psr\Container\ContainerInterface;
+use Psr\Log\LoggerInterface;
 use React\EventLoop\LoopInterface;
 use Recoil\Kernel;
 use Recoil\React\ReactKernel;
 use ReflectionClass;
 use Roave\BetterReflection\BetterReflection;
+use Symfony\Component\Console\Input\ArgvInput;
+use WyriHaximus\PSR3\CallableThrowableLogger\CallableThrowableLogger;
 
 final class CommandCreator
 {
-    /**
-     * @var LoopInterface
-     */
-    private $loop;
-
-    /**
-     * @var ContainerInterface
-     */
-    private $container;
-
     /**
      * @var Kernel
      */
     private $recoil;
 
     /**
-     * @param LoopInterface      $loop
+     * @var LoopInterface
+     */
+    private $logger;
+
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+
+    /**
+     * @param Kernel             $kernel
+     * @param LoggerInterface    $logger
      * @param ContainerInterface $container
      */
-    public function __construct(LoopInterface $loop, ContainerInterface $container)
+    public function __construct(Kernel $kernel, LoggerInterface $logger, ContainerInterface $container)
     {
-        $this->loop = $loop;
+        $this->recoil = $kernel;
+        $this->logger = $logger;
         $this->container = $container;
-        $this->recoil = ReactKernel::create($loop);
     }
 
     public function create(string $class): array
     {
         $container = $this->container;
         $recoil = $this->recoil;
+        $logger = $this->logger;
         $command = (new ReflectionClass($class))->getConstant('COMMAND');
         $parameters = [];
         foreach ((new BetterReflection())->classReflector()->reflect($class)->getMethod('__invoke')->getParameters() as $parameter) {
             $parameters[] = ((string)$parameter->getType()) . ' ' . ($parameter->isVariadic() ? '...' : '') . '$' . $parameter->getName();
         }
 
-        $eval = 'return function (' . implode(', ', $parameters) . ') use ($class, $container, $recoil) {
+        $eval = 'return function (' . implode(', ', $parameters) . ') use ($class, $container, $recoil, $logger) {
             $command = $container->get($class);
             $args = func_get_args();
-            $recoil->execute(function () use ($command, $args) {
-                yield $command(...$args);
+            $recoil->execute(function () use ($command, $args, $logger) {
+                try {
+                    yield $command(...$args);
+                } catch (\Throwable $et) {
+                    \WyriHaximus\PSR3\CallableThrowableLogger\CallableThrowableLogger::create($logger)($et);
+                }
             });
         };';
 
