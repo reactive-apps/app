30038e4efb584628d9132245eb63b94cb35cb0f2
Cees-Jan Kiewiet <ceesjank@gmail.com>
2019-08-14 08:26:52 +0200

    CS fix

--- tests/ContainerFactoryTest.php
+++ tests/ContainerFactoryTest.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace ReactiveApps\Tests;
 
@@ -6,16 +6,19 @@ use PHPUnit\Framework\TestCase;
 use Psr\Container\ContainerInterface;
 use ReactiveApps\ContainerFactory;
 
+/**
+ * @internal
+ */
 final class ContainerFactoryTest extends TestCase
 {
-    public function testConfig()
+    public function testConfig(): void
     {
         /** @var ContainerInterface $container */
         $container = ContainerFactory::create();
-/*var_export([$container]);
-        self::assertTrue($container->has('config'));
-        self::assertTrue($container->has('config.foo'));
-        self::assertTrue($container->has('config.foo.bar'));*/
+        /*var_export([$container]);
+                self::assertTrue($container->has('config'));
+                self::assertTrue($container->has('config.foo'));
+                self::assertTrue($container->has('config.foo.bar'));*/
         self::assertEquals('baz', $container->get('config.foo.bar'));
         self::assertEquals('baz', $container->get('config.foo.bar'));
     }
